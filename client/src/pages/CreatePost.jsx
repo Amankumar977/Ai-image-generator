367324cad8c0d309f8fbd68606897da1c09bd6f4
Aman Kumar <ak9330674@gmail.com>
2024-02-26 13:23:15 +0530

    Made the change in the backend route

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -51,15 +51,18 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGenerateImg(true);
-        const response = await fetch("http://localhost:5000/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            prompt: form.prompt,
-          }),
-        });
+        const response = await fetch(
+          "https://aiimage-fqdx.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              prompt: form.prompt,
+            }),
+          }
+        );
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (err) {
